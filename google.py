8ac1c2ab13a0fb22fb548db3eef5fc31053a200a
seung <sjoh0704@daum.net>
2021-11-25 01:24:57 +0900

    fix error

--- google.py
+++ google.py
@@ -6,7 +6,7 @@ import os
 from concurrent import futures
 
 def make_folder(directory):
-    print("{0} 디렉토리 생성".format(directory))
+    print("images/{0} 디렉토리 생성".format(directory))
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -14,7 +14,7 @@ def make_folder(directory):
         print ('Error: Creating directory. ' +  directory)
 
 def image_crawling(word):
-    make_folder(word)
+    make_folder("images/"+word)
     path='/home/seung/projects/image/chromedriver' 
     driver = webdriver.Chrome(path)
     driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
@@ -30,8 +30,8 @@ def image_crawling(word):
             opener=urllib.request.build_opener()
             opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
             urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(imgUrl, "{0}/{1}.jpg".format(word, i+1))
-            print("{0} image{} 생성".format(word, i+1))
+            urllib.request.urlretrieve(imgUrl, "images/{0}/{1}.jpg".format(word, i+1))
+            print("{0} image{1} 생성".format(word, i+1))
         except Exception as e:
             print(e)
             
@@ -40,19 +40,9 @@ def image_crawling(word):
     return "---------{} download done---------".format(word)
 
 if __name__ == "__main__":
+    make_folder("images")
 
     search = ["고양이", "강아지"]
-    # thread_list = []
-    # for s in search:
-    #     tmp = Thread(target=image_crawling, args=(s))
-    #     tmp.start()
-    #     thread_list.append(tmp)
-    # print(thread_list)
-    # for thread in thread_list:
-    #     print('asdf')
-    #     thread.join()
-    # image_crawling("고양이")
-
     with futures.ThreadPoolExecutor() as executor:
         results = [executor.submit(image_crawling, s) for s in search]
  
